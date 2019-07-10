a1c1f9b2e4a16362b4eb2aaee0d75eff0aa101af
Shibiliya <ismailshibiliya@gmail.com>
2019-07-10 15:40:03 +0530

    A new venture

---
+++ blog/posts/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from posts.models import Category
+from posts.models import Post
+
+# Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Post)
---
+++ blog/posts/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render , redirect
+from .models import Post
+from .forms import PostForm
+
+
+# Create your views here.
+def post_list(request):
+    posts=Post.objects.filter(is_active=True)
+    return render(request,'posts/post_list.html',{"posts":posts})
+
+def post_detail(request, pk):
+    post = Post.objects.get(id=pk)
+    return render(request,'posts/post_detail.html', {"post":post}) 
+
+def new_post(request):
+    if request.method == "POST" :
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+        return render(request, 'posts/new_post.html', {'form': form})
